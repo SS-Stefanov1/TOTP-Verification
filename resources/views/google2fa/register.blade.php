35b5f9a115fcbfa4329474ec91299f3f9a61889b
Stefan Stefanov <ss.business.dvem@gmail.com>
2023-09-02 11:21:04 +0300

    v1.01
    - Small visual tweaks

--- resources/views/google2fa/register.blade.php
+++ resources/views/google2fa/register.blade.php
@@ -5,14 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-8 justify-content-center">
             <div class="card">
-                <div class="card-header">Set up Google Authenticator</div>
-
-                <div class="card-body">
-                    <p>Set up your two factor authentication by scanning the barcode below. Alternatively, you can use the code {{ $secret }}</p>
+                <div class="card-header" style="text-align: center;">Set up Google Authenticator</div>
+                <div class="card-body" style="text-align: center;">
+                    <p>Set up your two factor authentication by scanning the barcode below.</p>
                     <div>
-                        <img src="{{ $QR_Image }}">
+                        {!! QrCode::size(150)->generate('Still in progress :D'); !!}
+
                     </div>
-                    <p>You must set up your Google Authenticator app before continuing. You will be unable to login otherwise</p>
+                    <p style="margin-top: 12px;">You must set up your Google Authenticator app before continuing. </br><u>You will be unable to login otherwise!</u></p>
                     <div>
                         <a href="/TotpTask/public/complete-registration"><button class="btn btn-primary">Complete Registration</button></a>
                     </div>
